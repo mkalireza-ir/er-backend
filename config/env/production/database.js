2519532cbacecd73d81b2f95d8f48f79f8be39d9
ali <websila.ir@gmail.com>
2023-02-24 19:38:36 +0330

    ready to deploy

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,7 +7,10 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL", false),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
+    debug: false,
   },
 });
